08b7f4d28545a8b1b960b67a7d95dd52c3ccba1d
Sebflatbeat <sebastien.darre@gmail.com>
2020-02-13 17:53:47 +0100

    SDA + Implementation de Zipkin

---
+++ zuul-server/src/main/java/com/BibliothequeP7/zuulserver/configurations/SleuthConfig.java
@@ -0,0 +1,12 @@
+package com.BibliothequeP7.zuulserver.configurations;
+
+import brave.sampler.Sampler;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SleuthConfig {
+
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
+    }
+}
