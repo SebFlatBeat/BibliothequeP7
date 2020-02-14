8d4676b815b8cdfad7ec5062d2f816682ed28a1f
Sebflatbeat <sebastien.darre@gmail.com>
2020-02-14 10:36:23 +0100

    SDA ajout de Spring Actuator et Spring Admin

---
+++ springadmin/src/test/java/com/BibliothequeP7/springadmin/SpringadminApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BibliothequeP7.springadmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringadminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
