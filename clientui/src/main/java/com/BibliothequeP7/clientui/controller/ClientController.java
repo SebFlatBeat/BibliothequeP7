e22978ad9fec464f497b8715ecead77a301c4a6e
Sebflatbeat <sebastien.darre@gmail.com>
2020-02-14 16:11:33 +0100

    ajout du package web dans ClientUI

--- clientui/src/main/java/com/BibliothequeP7/clientui/controller/ClientController.java
+++ clientui/src/main/java/com/BibliothequeP7/clientui/controller/ClientController.java
@@ -3,6 +3,7 @@ package com.BibliothequeP7.clientui.controller;
 import com.BibliothequeP7.clientui.proxies.UserProxy;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,10 +12,10 @@ public class ClientController {
     @Autowired
     private UserProxy userProxy;
 
-    @RequestMapping("/home")
+    @GetMapping("/index")
     public String homePage() {
 
-        return "home";
+        return "index";
     }
 
 
---
+++ clientui/src/main/java/com/BibliothequeP7/clientui/web/WebConfiguration.java
@@ -0,0 +1,38 @@
+package com.BibliothequeP7.clientui.web;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.web.config.EnableSpringDataWebSupport;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+
+@Configuration
+@EnableWebMvc
+@EnableSpringDataWebSupport
+@ComponentScan
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setPrefix("/resources/templates");
+        resolver.setSuffix(".html");
+        resolver.setViewClass(JstlView.class);
+        registry.viewResolver(resolver);
+    }
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/"
+    };
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS);
+    }
+}
