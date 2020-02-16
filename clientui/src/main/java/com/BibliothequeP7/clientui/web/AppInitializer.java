c97527e37c29a8a0a2460af3f1e57810be1d16bc
Sebflatbeat <sebastien.darre@gmail.com>
2020-02-16 17:24:10 +0100

    SDA + Suppression d'annotation dans WebConfiguration

--- clientui/src/main/java/com/BibliothequeP7/clientui/web/AppInitializer.java
+++ clientui/src/main/java/com/BibliothequeP7/clientui/web/AppInitializer.java
@@ -1,4 +1,25 @@
 package com.BibliothequeP7.clientui.web;
 
-public class AppInitializer {
+import org.apache.catalina.core.ApplicationContext;
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.ContextLoaderListener;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class AppInitializer implements WebApplicationInitializer {
+    @Override
+    public void onStartup(ServletContext servletContext) throws ServletException {
+        AnnotationConfigWebApplicationContext context = new AnnotationConfigWebApplicationContext();
+        context.scan("com.BibliothequeP7.clientui");
+        servletContext.addListener(new ContextLoaderListener(context));
+
+        ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("mvc", new DispatcherServlet(context));
+        dispatcher.setLoadOnStartup(1);
+        dispatcher.addMapping("/");
+    }
 }
