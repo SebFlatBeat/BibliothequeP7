9a0b5113c233ab78af64db2b5a253c7753017815
SebFlatBeat <sebastien.darre@gmail.com>
2020-03-31 20:13:16 +0200

    Correction Getter and Setter

--- user/src/main/java/com/BibliothequeP7/user/entities/UserBook.java
+++ user/src/main/java/com/BibliothequeP7/user/entities/UserBook.java
@@ -60,11 +60,11 @@ public class UserBook {
         this.id = id;
     }
 
-    public String getUserName() {
+    public String getUsername() {
         return username;
     }
 
-    public void setUserName(String username) {
+    public void setUsername(String username) {
         this.username = username;
     }
 
